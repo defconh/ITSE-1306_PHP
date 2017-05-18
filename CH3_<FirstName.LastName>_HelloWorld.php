a72f4b8d43c9d50c0ecfb7daba8efead587263b4
hwramsey <hramsey98@gmail.com>
2017-05-18 10:27:41 -0500

    CH3_<FirstName.LastName>_HelloWorld.php
    First Program

---
+++ CH3_<FirstName.LastName>_HelloWorld.php
@@ -0,0 +1,14 @@
+<?php
+//Made by Hayden Ramsey
+/* Hayden Ramsey
+ITSE 1306
+May 18th, 2017
+Chapter 3 PHP Code Assignment
+/*
+echo "Hello World!";
+$studentname = "Hayden";
+echo $studentname;
+$array = array(array('1','2','3'),
+               array('4','5','6'),
+               array('7','8','9'));
+echo $array[1][2];
