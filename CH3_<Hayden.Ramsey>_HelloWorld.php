18b2e017728585d24994554d77e95595af08298b
hwramsey <hramsey98@gmail.com>
2017-05-30 17:26:25 -0500

    Update and rename CH3_<FirstName.LastName>_HelloWorld.php to CH3_<Hayden.Ramsey>_HelloWorld.php

---
+++ CH3_<Hayden.Ramsey>_HelloWorld.php
@@ -0,0 +1,36 @@
+<?php
+//CH3_<Hayden.Ramsey>_HelloWorld.php
+
+/*Hayden Ramsey
+ITSE 1306
+May 18th, 2015
+*/
+
+echo "Hello World!";
+echo "<br />";
+
+$studentName = "Hayden Ramsey";
+echo $studentName;
+
+$myArray= array(array('1','2','3'),
+				array('4','5','6'),
+				array('7','8','9'));
+echo "<br />";	
+		
+echo $myArray[1][1];
+echo "<br />";
+
+echo "My name is $studentName.";
+echo '<br />';
+
+echo substr($studentName, 0, 7);
+echo "<br />";
+
+$x = 30;
+$y = 20;
+$z = $x * $y;
+echo "$x mulitplied by $y equals $z.";
+echo "<br />";
+
+echo date("l") . "<br>";
+?>
