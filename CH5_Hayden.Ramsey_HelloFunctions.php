2b00114369397e76a01b26ca90af5c11d6afb55a
hwramsey <hramsey98@gmail.com>
2017-06-14 15:49:11 -0500

    Create CH5_Hayden.Ramsey_HelloFunctions.php

--- CH5_Hayden.Ramsey_HelloFunctions.php
+++ CH5_Hayden.Ramsey_HelloFunctions.php
@@ -16,13 +16,7 @@ class PotentialUser
 {
 	function User($username, $fname, $lname, $password, $pnumber, $email, $address)
 	{
-		$username = "hwramsey";
-		$fname = "Hayden";
-		$lname = "Ramsey";
-		$password = "*******";
-		$pnumber = "936-652-4543";
-		$email = "hramsey98@gmail.com";
-		$address = "CR 4804, Marshall Texas";
+		//code and stuff
 	}
 }
 
